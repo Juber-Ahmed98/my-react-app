c0b10360d47d31c80f552ce3b46951c77571c2d7
Juber98 <juber.ahmed@wolseley.co.uk>
2026-02-22 12:42:08 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Juber Ahmed</h1>
+      <p>My first React app.</p>
+      <img src={reactLogo} alt="React logo" width="100" />
+    </div>
+  )
+}
+
+export default App
